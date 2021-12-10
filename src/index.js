e9877f7a65a5e0c601c2de3f80f87022d3a165ab
KasiaHajduk <kasiahajduk2014@gmail.com>
2021-12-10 23:49:47 +0100

    1012-2349

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //import fetchCountries from './fetchCountries';
 
 import Notiflix from 'notiflix';
@@ -15,31 +17,35 @@ const countryInfo = document.querySelector(".country-info");
 
 
 function fetchCountries(name) {
-    return fetch("https://restcountries.com/v3.1/name/" + name + "?fields=name,population,languages,capital,flags&_sort=name"
+    return fetch("https://restcountries.com/v2/name/" + name + "?fields=name,population,languages,capital,flags",
         ).then((response) => {
             if (!response.ok) {
-                Notiflix.Notify.failure('Oops, there is no country with that name')
-                return;
-                }
+                throw new Error(response.status);
+            }
             return response.json();
+        })
+        .catch(error => {
+            Notiflix.Notify.failure('Oops, there is no country with that name.');
+            console.log('tutaj' + error);
+            countryInfo.innerHTML = "";
+            countryList.innerHTML = "";
         });
+        ;
     }
 
-function renderCountryList(countries) {
-    const howMany = countries.length;
-    console.log(howMany);
-    if (howMany > 10)
+function renderCountryListInfo(countries) {
+    if (countries.length > 10)
     {
         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         countryInfo.innerHTML = "";
         countryList.innerHTML = "";
     }
-    else if (2<=howMany & howMany<=10){
+    else if (countries.length >= 2 && countries.length <= 10){
         const countriesList = countries.map((country) => {
         return `
             <li>
                 <img src = "${country.flags.svg}" alt="flag" width="30px" height="20"/>
-                <span >${country.name.common}</span>
+                <span >${country.name}</span>
             </li>
              `;
         })
@@ -49,12 +55,15 @@ function renderCountryList(countries) {
     }
     else {
         const landInfo = countries.map((country) => {
-        return `
-            <img src = "${country.flags.svg}" alt="flag" width="30px" />
-            <span>${country.name.common}</span>
-            <p><b>Capital</b>: ${country.capital}</p>
+            console.log(country);
+            return `
+            <div style="font-size: 30px; align-items: center; margin: auto;">
+                <img src = "${country.flags.svg}" alt="flag" width="40" height="25"/>
+                <span><b>${country.name}</b></span>
+            </div>
+            <p><b>Capital</b>: ${country.capital}</p>
             <p><b>Population</b>: ${country.population}</p>
-            <p><b>Languages</b>: ${country.languages.lenght}</p>
+            <p><b>Languages</b>: ${country.languages.map(language => language.name)}</p>
         `;
         })
         countryInfo.innerHTML = landInfo;
@@ -63,15 +72,8 @@ function renderCountryList(countries) {
 }
 
 
-
-
-
-
-
-
-
-inputBox.addEventListener("input", debounce(() => {
-    const name = inputBox.value.trim();
+function eventHandler(event) {
+    const name = event.target.value.trim();
     console.log("w kliku");
     console.log("-"+inputBox.value+"-");
     console.log("-"+name+"-");
@@ -79,22 +81,34 @@ inputBox.addEventListener("input", debounce(() => {
         Notiflix.Notify.info('Enter the country name.');
         return;
     }
-
     
     fetchCountries(name)
         .then((countries) => {
-            renderCountryList(countries);
+            renderCountryListInfo(countries);
         })
-        .catch((error) => console.log(error));
-    },DEBOUNCE_DELAY)
-);
+        .catch((error) => {
+            Notiflix.Notify.failure('Oops, there is no country with that name.');
+            console.log('tutaj' + error);
+            countryInfo.innerHTML = "";
+            countryList.innerHTML = "";
+        });
+    }
 
+
+inputBox.addEventListener("input", debounce(eventHandler, DEBOUNCE_DELAY));
+    
 
 
 
 //INTERFEJS
+document.body.style.backgroundImage = 'radial-gradient(#cfd0d2 1%, transparent 10%)';
+document.body.style.backgroundSize = '25px 25px';
+
+
 inputBox.style.fontSize = '20px';
 inputBox.style.marginLeft = '40px';
+inputBox.style.border = '1px solid #12428a';
+inputBox.style.borderRadius = '2px';
 
 //countryList.style.marginLeft = '20px';
 countryList.style.listStyle = 'none';
@@ -102,8 +116,5 @@ countryList.style.display = 'flex';
 countryList.style.flexDirection = 'column';
 countryList.style.fontSize = '20px';
 
-
-
-
-countryInfo.style.marginLeft = '20px';
-//countryInfo.span.style.fontSize = '80px';
+countryInfo.style.marginLeft = '40px';
+countryInfo.style.fontSize = '20px';
